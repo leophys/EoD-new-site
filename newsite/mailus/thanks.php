4ac164c4e4f6ed2f7723549c426487413b34343b
leophys <leo.barcaroli@gmail.com>
2018-05-25 17:40:06 +0200

    Mailus module

---
+++ newsite/mailus/thanks.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>EoD thank you!</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!--[if lte IE 8]><script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
+        <link rel="stylesheet" href="../assets/css/main.css" />
+        <!--[if lte IE 8]><link rel="stylesheet" href="../assets/css/ie8.css" /><![endif]-->
+        <meta http-equiv="refresh" content="5;url=http://embraceofdisharmony.com">
+    </head>
+    <body>
+        <section id="header">
+            <header>
+                <h1>Thank you!</h1>
+                <p>
+                    You will be contacted soon!
+                    <br/>
+                    <small>...hopefully</small>
+                </p>
+            </header>
+        </section>
+    </body>
+</html>
